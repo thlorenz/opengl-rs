f81eae4839bab02dc0b288b007ba9a2303298108
Thorsten Lorenz <thlorenz@gmx.de>
2020-07-28 19:17:07 -0600

    pull out init window

--- src/01_getting_started/02_hello_window_clear.rs
+++ src/01_getting_started/02_hello_window_clear.rs
@@ -1,37 +1,15 @@
-mod common;
+mod chapter;
 extern crate glfw;
 
 use glfw::Context;
 
 extern crate gl;
 
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 600;
-
 pub fn main() {
-    let mut ctx = glfw::init(glfw::FAIL_ON_ERRORS.clone()).unwrap();
-    ctx.window_hint(glfw::WindowHint::ContextVersion(3, 3));
-    ctx.window_hint(glfw::WindowHint::OpenGlProfile(
-        glfw::OpenGlProfileHint::Core,
-    ));
-    ctx.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
-
-    let (mut window, events) = ctx
-        .create_window(
-            SCREEN_WIDTH,
-            SCREEN_HEIGHT,
-            "Learn OpenGL",
-            glfw::WindowMode::Windowed,
-        )
-        .expect("Create Window");
-    window.make_current();
-    window.set_key_polling(true);
-    window.set_framebuffer_size_polling(true);
-
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+    let (mut ctx, mut window, events) = chapter::init_window();
 
     while !window.should_close() {
-        common::process_events(&mut window, &events);
+        chapter::process_events(&mut window, &events);
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
--- src/01_getting_started/chapter.rs
+++ src/01_getting_started/chapter.rs
@@ -1,15 +1,15 @@
-mod chapter;
-
+extern crate gl;
 extern crate glfw;
 
-use glfw::{Context, Key};
+use glfw::{Action, Key};
+use std::sync::mpsc::Receiver;
 
-extern crate gl;
+use glfw::{Context, Glfw, Window, WindowEvent};
 
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
 
-pub fn main() {
+pub fn init_window() -> (Glfw, Window, Receiver<(f64, WindowEvent)>) {
     let mut ctx = glfw::init(glfw::FAIL_ON_ERRORS.clone()).unwrap();
     ctx.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     ctx.window_hint(glfw::WindowHint::OpenGlProfile(
@@ -31,9 +31,19 @@ pub fn main() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    while !window.should_close() {
-        window.swap_buffers();
-        ctx.poll_events();
-        chapter::process_events(&mut window, &events);
+    (ctx, window, events)
+}
+
+pub fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                gl::Viewport(0, 0, width, height)
+            },
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            _ => {}
+        }
     }
 }
