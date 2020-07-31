123f594f309e0b71ec264e39e16061fbcd7db3db
Thorsten Lorenz <thlorenz@gmx.de>
2020-07-30 19:02:37 -0600

    shader: interpolation exercises

---
+++ src/01_getting_started/03_2_shaders_interpolation_ex1.rs
@@ -0,0 +1,47 @@
+mod chapter;
+extern crate glfw;
+
+use glfw::Context;
+
+extern crate gl;
+
+use opengl::shader::Shader;
+
+fn create_colored_triangle_vao() -> u32 {
+    #[rustfmt::skip]
+    let vertices: [f32;18] = [
+        // position        // color
+        0.5,  0.5, 0.0,    1.0, 0.0, 0.0,  // top right
+       -0.5,  0.5, 0.0,    0.0, 1.0, 0.0,  // top left
+        0.0, -0.5, 0.0,    0.0, 0.0, 1.0,  // bottom center
+    ];
+    chapter::create_vertices_vao(&vertices, 6)
+}
+
+pub fn main() {
+    let (mut ctx, mut window, events) = chapter::init_window();
+
+    let shader = Shader::new(
+        "src/01_getting_started/03_2_shaders_interpolation.vert",
+        "src/01_getting_started/03_2_shaders_interpolation.frag",
+    )
+    .expect("Failed to create shader");
+
+    let vao = create_colored_triangle_vao();
+    unsafe {
+        shader.use_program();
+    }
+
+    while !window.should_close() {
+        chapter::process_events(&mut window, &events);
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+        window.swap_buffers();
+        ctx.poll_events();
+    }
+}
---
+++ src/01_getting_started/03_2_shaders_interpolation_ex2.vert
@@ -0,0 +1,13 @@
+#version 330 core
+
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 color;
+
+uniform float dx;
+
+out vec3 ourColor;
+
+void main() {
+  gl_Position = vec4(pos.x + dx, pos.y, pos.z, 1.0);
+  ourColor = color;
+}
