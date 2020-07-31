efedfa843b5bd69775432aebaa2b2c76ebd9e0df
Thorsten Lorenz <thlorenz@gmx.de>
2020-07-30 22:11:15 -0600

    textures: rectangular texture

---
+++ src/01_getting_started/04_1_textures.rs
@@ -0,0 +1,59 @@
+mod chapter;
+extern crate glfw;
+
+use glfw::Context;
+
+extern crate gl;
+
+use opengl::shader::Shader;
+use std::ptr;
+
+pub fn create_rectangle_vao() -> u32 {
+    #[rustfmt::skip]
+    let vertices: [f32;32] = [
+    //    positions           colors       texture
+        0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
+        0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
+       -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom left
+       -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top left
+    ];
+    #[rustfmt::skip]
+    let indices = [
+        0, 1, 3,
+        1, 2, 3
+    ];
+    chapter::create_indexed_vertices_vao(&vertices, &indices, 8)
+}
+
+pub fn main() {
+    let (mut ctx, mut window, events) = chapter::init_window();
+
+    let shader = Shader::new(
+        "src/01_getting_started/04_1_textures.vert",
+        "src/01_getting_started/04_1_textures.frag",
+    )
+    .expect("Failed to create shader");
+
+    let vao = create_rectangle_vao();
+
+    let texture = chapter::load_texture("resources/textures/container.jpg");
+
+    unsafe {
+        gl::BindTexture(gl::TEXTURE_2D, texture);
+        shader.use_program();
+    }
+
+    window.set_focus_on_show(true);
+    while !window.should_close() {
+        chapter::process_events(&mut window, &events);
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::BindVertexArray(vao);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+        }
+        window.swap_buffers();
+        ctx.poll_events();
+    }
+}
--- src/01_getting_started/chapter.rs
+++ src/01_getting_started/chapter.rs
@@ -5,11 +5,13 @@ use std::sync::mpsc::Receiver;
 
 use glfw::{Context, Glfw, Window, WindowEvent};
 
+extern crate gl;
+use gl::types::*;
+
 use std::str;
 use std::{mem, ptr};
 
-extern crate gl;
-use gl::types::*;
+use image::GenericImageView;
 use std::ffi::c_void;
 
 const SCREEN_WIDTH: u32 = 800;
@@ -94,6 +96,7 @@ pub fn check_for_errors(item: u32, status_type: u32) {
     }
 }
 
+#[allow(dead_code)]
 pub fn create_vertices_vao(vertices: &[f32], el_size: i32) -> u32 {
     let has_color = el_size >= 6;
     let stride = el_size * mem::size_of::<GLfloat>() as GLsizei;
@@ -125,12 +128,107 @@ pub fn create_vertices_vao(vertices: &[f32], el_size: i32) -> u32 {
                 );
                 gl::EnableVertexAttribArray(1);
             }
-
             gl::BindBuffer(gl::ARRAY_BUFFER, 0);
             gl::BindVertexArray(0);
         }
         vao
     }
+}
+
+#[allow(dead_code)]
+pub fn create_indexed_vertices_vao(vertices: &[f32], indices: &[u32], el_size: i32) -> u32 {
+    let has_color = el_size >= 6;
+    let has_texture = el_size >= 8;
+    let stride = el_size * mem::size_of::<GLfloat>() as GLsizei;
+    unsafe {
+        let (mut vbo, mut vao, mut ebo) = (0, 0, 0);
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+        gl::GenBuffers(1, &mut ebo);
+        {
+            gl::BindVertexArray(vao);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                &vertices[0] as *const f32 as *const c_void,
+                gl::STATIC_DRAW,
+            );
+
+            gl::BufferData(
+                gl::ELEMENT_ARRAY_BUFFER,
+                (indices.len() * mem::size_of::<GLuint>()) as GLsizeiptr,
+                &indices[0] as *const u32 as *const c_void,
+                gl::STATIC_DRAW,
+            );
+
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+            gl::EnableVertexAttribArray(0);
+
+            if has_color {
+                gl::VertexAttribPointer(
+                    1,
+                    3,
+                    gl::FLOAT,
+                    gl::FALSE,
+                    stride,
+                    (3 * mem::size_of::<GLfloat>() as GLsizei) as *const c_void,
+                );
+                gl::EnableVertexAttribArray(1);
+            }
+            if has_texture {
+                gl::VertexAttribPointer(
+                    2,
+                    2,
+                    gl::FLOAT,
+                    gl::FALSE,
+                    stride,
+                    (6 * mem::size_of::<GLfloat>() as GLsizei) as *const c_void,
+                );
+                gl::EnableVertexAttribArray(2);
+            }
+
+            gl::BindVertexArray(0);
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0);
+        }
+        vao
+    }
+}
+
+#[allow(dead_code)]
+pub fn load_texture(path: &str) -> u32 {
+    let mut texture = 0;
+    unsafe {
+        gl::GenTextures(1, &mut texture);
+        gl::BindTexture(gl::TEXTURE_2D, texture);
+
+        // Wrapping
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+
+        // Filtering
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+
+        let img = image::open(path).expect("Failed to load texture image");
+        let texture_data = img.to_bytes();
+        gl::TexImage2D(
+            gl::TEXTURE_2D,
+            0,
+            gl::RGB as i32,
+            img.width() as i32,
+            img.height() as i32,
+            0,
+            gl::RGB,
+            gl::UNSIGNED_BYTE,
+            &texture_data[0] as *const u8 as *const c_void,
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+    }
+    texture
 }
 
 #[allow(dead_code)]
