0b8f1a33eca21c009175e0b1a322160748b8cb4a
Thorsten Lorenz <thlorenz@gmx.de>
2020-08-02 12:44:10 -0600

    coords: multiple

---
+++ src/01_getting_started/06_3_coordinate_systems_multiple.rs
@@ -0,0 +1,88 @@
+mod chapter;
+extern crate glfw;
+
+use glfw::Context;
+
+extern crate gl;
+
+extern crate nalgebra_glm as glm;
+
+use opengl::shader::Shader;
+use std::ffi::CString;
+
+pub fn main() {
+    let (mut ctx, mut window, events) = chapter::init_window();
+
+    let shader = Shader::new(
+        "src/01_getting_started/06_coordinate_systems.vert",
+        "src/01_getting_started/06_coordinate_systems.frag",
+    )
+    .expect("Failed to create shader");
+
+    let vao = chapter::create_box_vao();
+
+    let container_texture = chapter::load_texture("resources/textures/container.jpg", false, false);
+    let smiley_texture = chapter::load_texture("resources/textures/awesomeface.png", false, true);
+
+    let view = glm::translate(&glm::Mat4::identity(), &glm::vec3(0.0, 0.0, -3.0));
+    let projection = glm::perspective(
+        chapter::SCREEN_WIDTH as f32 / chapter::SCREEN_HEIGHT as f32,
+        45.0_f32.to_radians(),
+        0.1,
+        100.0,
+    );
+
+    let cube_positions = [
+        glm::vec3(0.0, 0.0, 0.0),
+        glm::vec3(2.0, 5.0, -15.0),
+        glm::vec3(-1.5, -2.2, -2.5),
+        glm::vec3(-3.8, -2.0, -12.3),
+        glm::vec3(2.4, -0.4, -3.5),
+        glm::vec3(-1.7, 3.0, -7.5),
+        glm::vec3(1.3, -2.0, -2.5),
+        glm::vec3(1.5, 2.0, -2.5),
+        glm::vec3(1.5, 0.2, -1.5),
+        glm::vec3(-1.3, 1.0, -1.5),
+    ];
+
+    unsafe {
+        shader.use_program();
+
+        shader.set_int(&CString::new("containerTexture").unwrap(), 0);
+        shader.set_int(&CString::new("smileyTexture").unwrap(), 1);
+
+        gl::ActiveTexture(gl::TEXTURE0);
+        gl::BindTexture(gl::TEXTURE_2D, container_texture);
+        gl::ActiveTexture(gl::TEXTURE0 + 1);
+        gl::BindTexture(gl::TEXTURE_2D, smiley_texture);
+
+        shader.set_mat4(&CString::new("view").unwrap(), &view);
+        shader.set_mat4(&CString::new("projection").unwrap(), &projection);
+
+        gl::Enable(gl::DEPTH_TEST);
+    }
+
+    window.set_focus_on_show(true);
+    while !window.should_close() {
+        chapter::process_events(&mut window, &events);
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+            gl::BindVertexArray(vao);
+            for i in 0..cube_positions.len() {
+                let pos = &cube_positions[i];
+                let translated = glm::translate(&glm::Mat4::identity(), &pos);
+                let model = glm::rotate(
+                    &translated,
+                    (20.0 * i as f32).to_radians(),
+                    &glm::vec3(1.0, 0.3, 0.5).normalize(),
+                );
+                shader.set_mat4(&CString::new("model").unwrap(), &model);
+                gl::DrawArrays(gl::TRIANGLES, 0, 36);
+            }
+        }
+        window.swap_buffers();
+        ctx.poll_events();
+    }
+}
