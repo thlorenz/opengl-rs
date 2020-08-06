4a7d23b1a615f86f3f28a04e4cbe7d4c3342a979
Thorsten Lorenz <thlorenz@gmx.de>
2020-08-06 00:04:59 -0600

    ch2: adding cube generator including texture coords

--- src/ch02_lighting/mod.rs
+++ src/ch02_lighting/mod.rs
@@ -4,6 +4,113 @@ use gl::types::*;
 use std::ffi::c_void;
 use std::{mem, ptr};
 
+pub fn create_textured_cube_with_normals_and_lamp_vaos() -> (u32, u32) {
+    const NPOS: usize = 3;
+    const NNORM: usize = 3;
+    const NTEX: usize = 2;
+    const NROWS: usize = 6;
+    const NSIDES: usize = 6;
+    let stride = (NPOS + NNORM + NTEX) as i32 * mem::size_of::<GLfloat>() as GLsizei;
+    #[rustfmt::skip]
+    let vertices: [f32;  (NPOS + NNORM + NTEX) * NROWS * NSIDES] = [
+        //   position               normal            texture
+        -0.5, -0.5, -0.5,      0.0,  0.0, -1.0,      0.0,  0.0,
+         0.5, -0.5, -0.5,      0.0,  0.0, -1.0,      1.0,  0.0,
+         0.5,  0.5, -0.5,      0.0,  0.0, -1.0,      1.0,  1.0,
+         0.5,  0.5, -0.5,      0.0,  0.0, -1.0,      1.0,  1.0,
+        -0.5,  0.5, -0.5,      0.0,  0.0, -1.0,      0.0,  1.0,
+        -0.5, -0.5, -0.5,      0.0,  0.0, -1.0,      0.0,  0.0,
+
+        -0.5, -0.5,  0.5,      0.0,  0.0,  1.0,      0.0,  0.0,
+         0.5, -0.5,  0.5,      0.0,  0.0,  1.0,      1.0,  0.0,
+         0.5,  0.5,  0.5,      0.0,  0.0,  1.0,      1.0,  1.0,
+         0.5,  0.5,  0.5,      0.0,  0.0,  1.0,      1.0,  1.0,
+        -0.5,  0.5,  0.5,      0.0,  0.0,  1.0,      0.0,  1.0,
+        -0.5, -0.5,  0.5,      0.0,  0.0,  1.0,      0.0,  0.0,
+
+        -0.5,  0.5,  0.5,     -1.0,  0.0,  0.0,      1.0,  0.0,
+        -0.5,  0.5, -0.5,     -1.0,  0.0,  0.0,      1.0,  1.0,
+        -0.5, -0.5, -0.5,     -1.0,  0.0,  0.0,      0.0,  1.0,
+        -0.5, -0.5, -0.5,     -1.0,  0.0,  0.0,      0.0,  1.0,
+        -0.5, -0.5,  0.5,     -1.0,  0.0,  0.0,      0.0,  0.0,
+        -0.5,  0.5,  0.5,     -1.0,  0.0,  0.0,      1.0,  0.0,
+
+         0.5,  0.5,  0.5,      1.0,  0.0,  0.0,      1.0,  0.0,
+         0.5,  0.5, -0.5,      1.0,  0.0,  0.0,      1.0,  1.0,
+         0.5, -0.5, -0.5,      1.0,  0.0,  0.0,      0.0,  1.0,
+         0.5, -0.5, -0.5,      1.0,  0.0,  0.0,      0.0,  1.0,
+         0.5, -0.5,  0.5,      1.0,  0.0,  0.0,      0.0,  0.0,
+         0.5,  0.5,  0.5,      1.0,  0.0,  0.0,      1.0,  0.0,
+
+        -0.5, -0.5, -0.5,      0.0, -1.0,  0.0,      0.0,  1.0,
+         0.5, -0.5, -0.5,      0.0, -1.0,  0.0,      1.0,  1.0,
+         0.5, -0.5,  0.5,      0.0, -1.0,  0.0,      1.0,  0.0,
+         0.5, -0.5,  0.5,      0.0, -1.0,  0.0,      1.0,  0.0,
+        -0.5, -0.5,  0.5,      0.0, -1.0,  0.0,      0.0,  0.0,
+        -0.5, -0.5, -0.5,      0.0, -1.0,  0.0,      0.0,  1.0,
+
+        -0.5,  0.5, -0.5,      0.0,  1.0,  0.0,      0.0,  1.0,
+         0.5,  0.5, -0.5,      0.0,  1.0,  0.0,      1.0,  1.0,
+         0.5,  0.5,  0.5,      0.0,  1.0,  0.0,      1.0,  0.0,
+         0.5,  0.5,  0.5,      0.0,  1.0,  0.0,      1.0,  0.0,
+        -0.5,  0.5,  0.5,      0.0,  1.0,  0.0,      0.0,  0.0,
+        -0.5,  0.5, -0.5,      0.0,  1.0,  0.0,      0.0,  1.0
+    ];
+
+    unsafe {
+        let (mut vbo, mut cube_vao, mut lamp_vao) = (0, 0, 0);
+        gl::GenBuffers(1, &mut vbo);
+        gl::GenVertexArrays(1, &mut cube_vao);
+        gl::GenVertexArrays(1, &mut lamp_vao);
+        {
+            // load vertices into vbo
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                &vertices[0] as *const f32 as *const c_void,
+                gl::STATIC_DRAW,
+            );
+
+            // cube vao
+            gl::BindVertexArray(cube_vao);
+            gl::VertexAttribPointer(0, NPOS as i32, gl::FLOAT, gl::FALSE, stride, ptr::null());
+            gl::EnableVertexAttribArray(0);
+
+            // normal attrib
+            gl::VertexAttribPointer(
+                1,
+                NNORM as i32,
+                gl::FLOAT,
+                gl::FALSE,
+                stride,
+                (NPOS as i32 * mem::size_of::<GLfloat>() as GLsizei) as *const c_void,
+            );
+            gl::EnableVertexAttribArray(1);
+
+            // texture attrib
+            gl::VertexAttribPointer(
+                2,
+                NTEX as i32,
+                gl::FLOAT,
+                gl::FALSE,
+                stride,
+                ((NPOS + NNORM) as i32 * mem::size_of::<GLfloat>() as GLsizei) as *const c_void,
+            );
+            gl::EnableVertexAttribArray(2);
+
+            // lamp vao
+            gl::BindVertexArray(lamp_vao);
+            gl::VertexAttribPointer(0, NPOS as i32, gl::FLOAT, gl::FALSE, stride, ptr::null());
+            gl::EnableVertexAttribArray(0);
+
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+            gl::BindVertexArray(0);
+        }
+        (cube_vao, lamp_vao)
+    }
+}
+
 pub fn create_cube_with_normals_and_lamp_vaos() -> (u32, u32) {
     const NPOS: usize = 3;
     const NNORM: usize = 3;
@@ -98,6 +205,7 @@ pub fn create_cube_with_normals_and_lamp_vaos() -> (u32, u32) {
         (cube_vao, lamp_vao)
     }
 }
+
 pub fn create_box_vao() -> u32 {
     const NPOS: usize = 3;
     const NROWS: usize = 6;
