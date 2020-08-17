40242443e6ebedb1a213c7bfd9bf0b3e6eb71cdd
Thorsten Lorenz <thlorenz@gmx.de>
2020-08-16 19:58:42 -0600

    ch04: blending discard

--- src/ch04_advanced_opengl/mod.rs
+++ src/ch04_advanced_opengl/mod.rs
@@ -1,9 +1,10 @@
 extern crate gl;
-use gl::types::*;
-use std::ffi::c_void;
-use std::ptr;
 
+use gl::types::*;
+use nalgebra_glm as glm;
+use std::ffi::c_void;
 use std::mem;
+use std::ptr;
 
 pub fn create_textured_cube_vao() -> u32 {
     let (npos, ntex, stride, vertices) = vertices_box_pos_tex();
@@ -12,6 +13,22 @@ pub fn create_textured_cube_vao() -> u32 {
 
 pub fn create_textured_plane_vao() -> u32 {
     let (npos, ntex, stride, vertices) = vertices_plane_pos_tex();
+    create_textured_vao(npos, ntex, stride, vertices.to_vec())
+}
+
+#[rustfmt::skip]
+pub fn vec3_vegetation() -> [glm::Vec3; 5] {
+    [
+        glm::vec3(-1.5, 0.0, -0.48),
+        glm::vec3( 1.5, 0.0,  0.51),
+        glm::vec3( 0.0, 0.0,  0.7),
+        glm::vec3(-0.3, 0.0, -2.3),
+        glm::vec3( 0.5, 0.0, -0.6),
+    ]
+}
+
+pub fn create_textured_transparent_vao() -> u32 {
+    let (npos, ntex, stride, vertices) = vertices_transparent_pos_tex();
     create_textured_vao(npos, ntex, stride, vertices.to_vec())
 }
 
@@ -48,6 +65,27 @@ fn create_textured_vao(npos: usize, ntex: usize, stride: i32, vertices: Vec<f32>
         }
         cube_vao
     }
+}
+
+fn vertices_transparent_pos_tex() -> (usize, usize, i32, [f32; (3 + 2) * 1 * 6]) {
+    const NPOS: usize = 3;
+    const NTEX: usize = 2;
+    const NROWS: usize = 1;
+    const NSIDES: usize = 6;
+    let stride = (NPOS + NTEX) as i32 * mem::size_of::<GLfloat>() as GLsizei;
+
+    #[rustfmt::skip]
+    let vertices: [f32;  (NPOS + NTEX) * NROWS * NSIDES] = [
+        //   position           texture
+         0.0,  0.5,  0.0,      0.0,  1.0,
+         0.0, -0.5,  0.0,      0.0,  0.0,
+         1.0, -0.5,  0.0,      1.0,  0.0,
+
+         0.0,  0.5,  0.0,      0.0,  1.0,
+         1.0, -0.5,  0.0,      1.0,  0.0,
+         1.0,  0.5,  0.0,      1.0,  1.0
+    ];
+    (NPOS, NTEX, stride, vertices)
 }
 
 fn vertices_plane_pos_tex() -> (usize, usize, i32, [f32; (3 + 2) * 1 * 6]) {
@@ -124,4 +162,3 @@ fn vertices_box_pos_tex() -> (usize, usize, i32, [f32; (3 + 2) * 6 * 6]) {
     ];
     (NPOS, NTEX, stride, vertices)
 }
-
